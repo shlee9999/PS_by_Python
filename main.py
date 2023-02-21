0f183e7d7d2fd6c14b10926f1c443d123b9965e6
Seonghoon Lee <oklshop555@naver.com>
2023-02-21 13:37:09 +0900

    올림픽

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-N, L = map(int, input().split())
-arr = list(map(int, input().split()))
-leak = [0] * (1001 + L)
-for i in arr:
-    leak[i] = 1
-l = r = 1
-length = 0
-result = 0
-while r <= 1000 + L:
-    if leak[r] == 1:
-        l = r
-        r += L
-        result += 1
+class Country:
+    r = 0
+
+    def __init__(self, num, gold, silver, bronze):
+        self.num = num
+        self.gold = gold
+        self.silver = silver
+        self.bronze = bronze
+
+    def rank(self, num):
+        self.r = num
+
+
+N, K = map(int, input().split())
+countries = [Country(0, 0, 0, 0)]
+
+for i in range(1, N + 1):
+    li = list(map(int, input().split()))
+    countries.append(Country(li[0], li[1], li[2], li[3]))
+sorted(countries, key=lambda x: (-x.gold, -x.silver, -x.bronze))
+
+cnt = 0
+i = 1
+while (i <= N):
+    cnt += 1
+    if i < N and countries[i].gold == countries[i + 1].gold and countries[i].silver == countries[i + 1].silver and \
+            countries[i].bronze == countries[i + 1].bronze:
+        countries[i].rank(cnt)
+        countries[i + 1].rank(cnt)
+        i += 1
+
     else:
-        r += 1
-print(int(result), end="")
+        countries[i].rank(cnt)
+    i += 1
+print(countries[K].r)
