386957ba630afc54292329e429b6131196057bd8
oklsh <oklshop555@naver.com>
2023-02-21 15:17:29 +0900

    올림픽

---
+++ BOJ/N8979.py
@@ -0,0 +1,23 @@
+# Gold 10**12+1~
+# Silver 10**6+1~
+# Bronze 1~
+class Country: #올림픽
+    def __init__(self, num, score):
+        self.num = num
+        self.score = score
+
+
+N, K = map(int, input().split())
+countries = []
+
+index = -1
+for i in range(N):
+    li = list(map(int, input().split()))
+    countries.append(Country(li[0], li[1] * 10 ** 12 + li[2] * 10 ** 6 + li[3]))
+    if li[0] == K:
+        index = i
+cnt = 0
+for i in range(N):
+    if countries[i].score > countries[index].score:
+        cnt += 1
+print(cnt + 1)
