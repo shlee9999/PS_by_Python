0fcfb2993df849d44f790288eedeb5d350b8657d
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-10-12 14:20:11 +0900

    [Silver III] Title: 분수 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.py
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -1,9 +1,12 @@
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 res = [num1[0] * num2[1] + num2[0] * num1[1], num1[1] * num2[1]]
-for i in range(min(res), 0, -1):
-    if res[0]%i == 0 and res[1]%i ==0:
-        print(res[0]//i, res[1]//i)
-        break
-
+G = gcd(res[0], res[1])
+print(res[0] // G, res[1] // G)
 
