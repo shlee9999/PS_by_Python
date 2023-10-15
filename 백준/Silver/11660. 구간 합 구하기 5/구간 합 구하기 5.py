76593f24726ad81db5eb97669526f4f591fe5947
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-10-15 17:17:56 +0900

    Update 구간 합 구하기 5.py

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,26 +1,12 @@
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-ran = [list(map(int, input().split())) for _ in range(m)]
+sum_dp = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        sum_dp[i][j] = sum_dp[i - 1][j] + sum_dp[i][j - 1] - sum_dp[i - 1][j - 1] + arr[i - 1][j - 1]
 
-enum_list = []
-
-for i in range(n):
-    li = []
-    for j in range(n):
-        if j == 0:
-            li.append(arr[i][0])
-        else:
-            li.append(arr[i][j] + li[j - 1])  # 누적 합
-    enum_list.append(li)
-
-for r in ran:
-    sum = 0
-    x1, y1, x2, y2 = r
-    left = y1 - 2
-    right = y2 - 1
-    for i in range(x1 - 1, x2):
-        if left == -1:
-            sum += enum_list[i][right]
-        else:
-            sum += enum_list[i][right] - enum_list[i][left]
-    print(sum)
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(sum_dp[x2][y2] - sum_dp[x1-1][y2] - sum_dp[x2][y1-1] + sum_dp[x1-1][y1-1])
