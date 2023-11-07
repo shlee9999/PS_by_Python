c78b5a69bb724427153161db6f820a15bcab1589
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-11-07 18:29:47 +0900

    [Gold V] Title: LCS, Time: 512 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+a = ' ' + input().rstrip()
+b = ' ' + input().rstrip()
+
+dp = [[0] * len(a) for _ in range(len(b))]
+for i in range(1, len(b)):
+    for j in range(1, len(a)):
+        if b[i] == a[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[-1][-1])
