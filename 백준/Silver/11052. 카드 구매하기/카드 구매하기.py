fe2f13bc876a56bbb31cb69ace1e904381724f88
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-11-18 15:34:17 +0900

    [Silver I] Title: 카드 구매하기, Time: 220 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,14 +1,10 @@
 n = int(input())
-p = [0]
+
 dp = [0] * (n + 1)
 li = list(map(int, input().split()))
-for i in range(1, n+1):
-    dp[i] = li[i-1]
+p = [0] + li
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if i * j <= n:
-            dp[i * j] = max(dp[i] * j, dp[i * j])
-        if i + j <= n:
-            dp[i + j] = max(dp[i] + dp[j], dp[i + j])
+for k in range(1, n + 1):
+    for i in range(1, k+1):
+        dp[k] = max(dp[k], dp[k - i] + p[i])
 print(dp[n])
