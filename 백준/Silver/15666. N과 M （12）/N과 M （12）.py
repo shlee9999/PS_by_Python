bd0b4b99f05caf72c88caba857c808d5feecdfdd
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-18 13:11:16 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+li = list(map(int, input().split()))
+li.sort()
+
+s = []
+
+visited = [False] * n
+
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    prev = 0
+    for i in range(n):
+        if prev != li[i]:  # prev를 이용해 다음에 올 숫자가 이미 확인했던 값이면 패스한다
+            if len(s) == 0 or len(s) > 0 and s[-1] <= li[i]:
+                prev = li[i]
+                s.append(li[i])
+                visited[i] = True
+                dfs()
+                s.pop()
+                visited[i] = False
+
+
+dfs()
