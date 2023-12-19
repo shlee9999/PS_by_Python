e29b5925ad7896c49cefe8461b6377e4440f7da3
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-19 10:43:48 +0900

    [Gold III] Title: 줄 세우기, Time: 4328 ms, Memory: 37784 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,31 +1,27 @@
 from collections import deque
 
+q = deque()
 n, m = map(int, input().split())
-indegree = [0] * (n + 1)
 graph = [[] for _ in range(n + 1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    indegree[b] += 1
+indegree = [0] * (n + 1)  # 진입 차수
+for _ in range(m):
+    node, next = map(int, input().split())
+    graph[node].append(next)
+    indegree[next] += 1
 
 
-def topological_sort(graph, indegree):
-    result = []
-    q = deque()
+def topological_sort():
     for i in range(1, n + 1):
         if indegree[i] == 0:
             q.append(i)
-
     while q:
-        now = q.popleft()
-        result.append(now)
-        for x in graph[now]:
+        node = q.popleft()
+        print(node, end=' ')
+        indegree[node] = -100
+        for x in graph[node]:
             indegree[x] -= 1
             if indegree[x] == 0:
                 q.append(x)
-    return result
 
 
-answer = topological_sort(graph, indegree)
-for x in answer:
-    print(x, end=' ')
+topological_sort()
