759ed3b21534d18a3643d1d4f26b4304d59e815b
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-22 07:59:28 +0900

    [Gold V] Title: 리모컨, Time: 2644 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -2,7 +2,6 @@ import sys
 
 input = sys.stdin.readline
 n = int(input())
-_str = str(n)
 m = int(input())
 
 if m == 0:
@@ -10,30 +9,16 @@ if m == 0:
 else:
     broken = list(map(int, input().split()))
 
-res = 100
-usable = [i for i in range(10) if i not in broken]
-
+res = 1000001
 diff = abs(res - n)  # 채널 이동 안함
 
+for num in range(1000001):
+    num = str(num)
+    for j in range(len(num)):
+        if int(num[j]) in broken:
+            break
+    else:
+        if len(num) + abs(int(num) - n) < len(str(res)) + abs(res - n):
+            res = int(num)
 
-def bt(depth, s):
-    global res
-    if depth == 7 or depth == len(_str) + 2:
-        return
-    if s != '' and -1 <= depth - len(_str) <= 1:
-        res_cnt = len(str(res)) + abs(res - n)
-        s_cnt = depth + abs(int(s) - n)
-        if res_cnt > s_cnt:  # 더 적게 누르면
-            res = int(s)
-    for i in range(len(usable)):
-        if s == '' and usable[i] == 0:  # 맨 앞자리가 0인 경우는 제외
-            res_cnt = len(str(res)) + abs(res - n)
-            s_cnt = 1 + n
-            if res_cnt > s_cnt:  # 더 적게 누르면
-                res = 0
-            continue
-        bt(depth + 1, s + str(usable[i]))
-
-
-bt(0, "")
-print(min(len(str(res)) + abs(res - n), diff))
+print(min(len(str(res)) + abs(res - n), abs(100 - n)))
