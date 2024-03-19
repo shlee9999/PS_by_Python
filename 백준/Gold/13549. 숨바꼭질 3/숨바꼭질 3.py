b1c9d8772c5fdd2971d84e8872f3167a59c88902
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-03-19 21:53:43 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 38380 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,17 +1,28 @@
 import sys
+from collections import deque
 
+INF = 100001
 input = sys.stdin.readline
 N, K = map(int, input().split())
-INF = 100001
 dp = [INF] * INF
-for i in range(N + 1):  # N 이하는 전부 N - i
-    dp[i] = N - i
-for i in range(N + 1, INF):
-    dp[i] = min(dp[i], dp[i // 2] + i % 2, dp[i - 1] + 1)
 
-for _ in range(10):
-    for i in range(N + 1, INF - 1):
-        dp[i] = min(dp[i], dp[i // 2] + i % 2, dp[i - 1] + 1, dp[i + 1] + 1)
-dp[INF - 1] = min(dp[INF - 2] + 1, dp[INF - 1])
 
+def bfs():
+    q = deque()
+    for i in range(N + 1):
+        dp[i] = N - i
+        q.append(i)
+    while q:
+        cur = q.popleft()
+        if N < cur + 1 < INF and dp[cur] + 1 < dp[cur + 1]:
+            dp[cur + 1] = dp[cur] + 1
+            q.append(cur + 1)
+        if N < cur - 1 < INF and dp[cur] + 1 < dp[cur - 1]:
+            dp[cur - 1] = dp[cur] + 1
+            q.append(cur - 1)
+        if N < cur * 2 < INF and dp[cur] < dp[cur * 2]:
+            dp[cur * 2] = dp[cur]
+            q.append(cur * 2)
+
+bfs()
 print(dp[K])
