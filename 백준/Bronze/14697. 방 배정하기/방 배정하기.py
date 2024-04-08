cfe8b3959c935badc6583614dc781370084b597c
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-04-08 15:09:58 +0900

    [Bronze II] Title: 방 배정하기, Time: 112 ms, Memory: 109240 KB, Score: 3 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -4,10 +4,11 @@ input = sys.stdin.readline
 
 a, b, c, n = map(int, input().split())
 
+flag = False
 for i in range(1, n // a + 1):
     for j in range(1, n // b + 1):
         for k in range(1, n // c + 1):
             if a * i + b * j + c * k == n:
-                print(1)
-                sys.exit()
-print(0)
+                flag = True
+
+print(1 if flag else 0)
